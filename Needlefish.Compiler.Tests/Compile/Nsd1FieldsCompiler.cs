fdc1129e97082baf925cb060f376090bf2384eb6
Seven <chase@chasehuegel.io>
2024-01-01 23:52:40 -0500

    fix compiled arrays missing type

--- Needlefish.Compiler.Tests/Compile/Nsd1FieldsCompiler.cs
+++ Needlefish.Compiler.Tests/Compile/Nsd1FieldsCompiler.cs
@@ -20,7 +20,7 @@ internal class Nsd1FieldsCompiler : INsdTypeCompiler
 
             if (fieldDefinition.IsArray)
             {
-                fullyQualifiedTypeStr = "[]";
+                fullyQualifiedTypeStr += "[]";
             }
 
             if (fieldDefinition.IsOptional)
